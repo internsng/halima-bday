3564b35570bc7fc6dc2c1162f007152bb6b56384
internsng <internshq@interns.ng>
2026-04-03 09:07:11 +0100

    inital commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,42 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="w-full h-full bg-[#F9F1F0]">
+      <header className="bg-[#FADCD9] mt-4 h-[50px] px-5 rounded-[16px] max-w-[95%] mx-auto flex items-center justify-end">
+        <div className="flex items-center gap-4 text-black font-normal text-base">
+          <Link href="/">Home</Link>
+          <Link href="/">Our Story</Link>
+          <Link href="/">The New Chapter</Link>
+        </div>
+      </header>
+
+      <div className="mt-10">
+        <div className="text-center">
+          <h1 className="text-2xl font-light text-black ">A small corner of the internet, built just to remind you how deeply I love you</h1>
+          <h1 className="text-[72px] font-normal text-black mt-2">Happy Birthday, <span className="font-semibold">Halima</span></h1>
+        </div>
+      </div>
+
+      <div className="mt-10">
+        <div className="max-w-[750px] h-[541px] bg-red-500 rounded-[50px] mx-auto">
+          
+        </div>
+        <p className="text-center text-black text-[28px] font-normal mt-5">You didn't just change my life... you brought a new one into it.</p>
+        <div className="max-w-[612px] mx-auto mt-5">
+          <p className="text-black text-xl font-extralight text-center">
+          You are calm in my chaos, clarity in my confusion, and light in places i didn't even know were dark.
+          You took everything i thought i understood about love and gave it a deeper meaning.
+          Noe, I don't just see love in how you care for me, I see it in how you hold, nurture, and protect the life we created together. 
+          And in that, you've given me not just more to love, but a whole new reason to love even harder.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+
+      <div className="mt-10">
+        <h1 className="text-center text-black text-[72px] font-normal">Our Story</h1>
+      </div>
     </div>
   );
 }
